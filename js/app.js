efed96dfffe3a981da196851d692a3d0d9027541
Sylwia <sylwia.lubiniecka@gmail.com>
2017-10-21 16:02:10 +0200

    zmiany

---
+++ js/app.js
@@ -0,0 +1,15 @@
+console.log("działa!")
+var myName = 'Sylwia';
+console.log('myName', myName);
+var friends = ['Gosia', 'Ania', 'Kasia', 'Iza'];
+console.log(friends[1]);
+var a = 8;
+var b = 4;
+if (a==b){
+    console.log("hurra");   
+}else { 
+console.log("wrrong");
+}
+for(var i=0; i<=friends.length-1; i++){
+    console.log(friends[i])};
+
